20f8333374d566e77d00a05559412ca33ec81a66
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2020-10-28 15:30:22 +0100

    first

---
+++ src/ba/unsa/rpr/tutorijal1/Main.java
@@ -0,0 +1,34 @@
+package ba.unsa.rpr.tutorijal1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite broj n");
+        int n = scanner.nextInt();
+
+        for(int i=2; i<n; i++) {
+            if(i%sumaCifara((i))==0) System.out.println(i);
+        }
+
+    }
+    
+
+
+
+
+
+    public static int sumaCifara(int n) {
+        int suma = 0;
+        while(n!=0) {
+            suma+=n%10;
+            n=n/10;
+        }
+
+        return suma;
+    }
+
+}
